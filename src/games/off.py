4fe9760bf0a5caabbfba361db0122a3245dd549f
Wamanuz <mackan19@hotmail.com>
2014-02-19 14:18:48 +0100

    Added a 'game' that turns off all the lights

---
+++ src/games/off.py
@@ -0,0 +1,27 @@
+import tornado.escape
+
+import lightgames
+
+
+def create(client):
+    print("Turning off lamps")
+    return Off(client)
+
+
+class Off(lightgames.Game):
+    """A 'game' used to easily turn off the lights when not needed"""
+    def reset(self):
+        print("New game!")
+
+        buffer = []
+        for y in range(3): # TODO Get these variables from the server 
+            for x in range(3):
+                buffer += [{'x':x, 'y':y, 'change':{ "on":False }}]
+        self.client.request("POST", "/lights", tornado.escape.json_encode(buffer))
+        print(self.client.getresponse().read().decode())
+
+    def init(self):
+        self.reset()
+
+
+
