623547d64aefd116706029d1fe4b610866def6c5
Arvid Fahlström Myrman <behold@behold.se>
2014-09-24 03:02:54 +0200

    Refactor GIF animation code (untested with automatic start of animation when no one is playing)

--- src/games/gifanimation.py
+++ src/games/gifanimation.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PIL import Image
 from tornado import gen
 from tornado.ioloop import IOLoop
 import time
@@ -45,7 +44,7 @@ class GifAnimation(lightgames.Game):
         self.template_vars['color_off'] = '#000000'
         
         self.play = False
-        self.data = open(self.template_vars['animation_file'], 'rb').read()
+        self.data = open(self.template_vars['animation_file'], 'rb')
 
         self.grid = lightgames.get_grid_size()
         self.template_vars['grid_x'], self.template_vars['grid_y'] = self.grid
@@ -63,95 +62,56 @@ class GifAnimation(lightgames.Game):
         # keep track of if the animation file has changed
         self.changed_gif = False
 
+        self.gif_animator = lightgames.GIFAnimation(self)
+
     def destroy(self):
         super().destroy()
         self.play = False
+        self.gif_animator.cancel()
+        self.gif_animator.close()
 
-    @gen.engine
     def play_animation(self):
         self.reset_lamp_all()
         self.play = True
 
-        while self.play:
-            try:
-                gif = Image.open(io.BytesIO(self.data))
-                (width, height) = gif.size
-                #self.template_vars['grid_x'] = width
-                #self.template_vars['grid_y'] = height
+        def on_frame(xy, color, is_transparent):
+            x, y = xy
+            lightgames.send_msgs(self.connections, \
+                        {'x': x, 'y': y, 'color': color, 'power': not is_transparent})
 
-                # Reset lamps if GIF file has changed
-                # since it might be of different dimensions
-                if self.changed_gif:
-                    self.reset_lamp_all()
-                    self.changed_gif = False
-                i = 0
-                for frame in ImageSequence(gif):
-                    # reset lamps when display settings has changed
-                    if self.changed_display:
-                        self.reset_lamp_all()
-                        self.changed_display = False
-                    # Break if animation turned off.
-                    # Change this accordingly to wait for animation
-                    # to finish before turning off/changing animation
-                    if not self.play:
-                        break
-                    tt = self.transition_time
-                    i = i+1
-                    rgb_im = gif.convert("RGB")
-                    # duration of frame is in milliseconds
-                    dur = gif.info['duration']
-                    buffer = []
-                    for y in range(0, min(height,self.grid[1])):
-                        for x in range(0, min(width,self.grid[0])):
+        try:
+            #for y in range(0, min(height,self.grid[1])):
+                #for x in range(0, min(width,self.grid[0])):
+                    ## center the image horizontally
+                    #x_im = x_grid = x
+                    #if self.center_hor and width > self.grid[0]:
+                        #x_im = int( (width - self.grid[0])/2 + x)
+                    #elif self.center_hor and width < self.grid[0]:
+                        #x_grid = int( (self.grid[0] - width)/2 + x)
+                    ## center the image vertically
+                    #y_im = y_grid = y
+                    #if self.center_vert and height > self.grid[1]:
+                        #y_im = int( (height - self.grid[1])/2 + y)
+                    #elif self.center_vert and height < self.grid[0]:
+                        #y_grid = int( (self.grid[0] - height)/2 + y)
+                    ## offset image horizontally
+                    #if self.offset_x != 0 and width <= self.grid[0]:
+                        #x_grid += self.offset_x
+                    #elif self.offset_x != 0 and width > self.grid[0]:
+                        #x_im -= self.offset_x
+                    ## offset image vertically
+                    #if self.offset_y != 0 and height <= self.grid[1]:
+                        #y_grid += self.offset_y
+                    #elif self.offset_y != 0 and height > self.grid[1]:
+                        #y_im -= self.offset_y
 
-                            # center the image horizontally
-                            x_im = x_grid = x
-                            if self.center_hor and width > self.grid[0]:
-                                x_im = int( (width - self.grid[0])/2 + x)
-                            elif self.center_hor and width < self.grid[0]:
-                                x_grid = int( (self.grid[0] - width)/2 + x)
-                            # center the image vertically
-                            y_im = y_grid = y 
-                            if self.center_vert and height > self.grid[1]:
-                                y_im = int( (height - self.grid[1])/2 + y)
-                            elif self.center_vert and height < self.grid[0]:
-                                y_grid = int( (self.grid[0] - height)/2 + y)
-                            # offset image horizontally
-                            if self.offset_x != 0 and width <= self.grid[0]:
-                                x_grid += self.offset_x
-                            elif self.offset_x != 0 and width > self.grid[0]:
-                                x_im -= self.offset_x
-                            # offset image vertically
-                            if self.offset_y != 0 and height <= self.grid[1]:
-                                y_grid += self.offset_y
-                            elif self.offset_y != 0 and height > self.grid[1]:
-                                y_im -= self.offset_y
-
-                            # check boundaries that might be off after offset
-                            if (0 <= x_im < width
-                            and 0 <= x_grid < self.grid[0]
-                            and 0 <= y_im < height
-                            and 0 <= y_grid < self.grid[1]):
-                                r, g, b = rgb_im.getpixel((x_im, y_im))
-                                if (r, g, b) != self.transp_color:
-                                    buffer += [{'x': x_grid, 'y': y_grid, \
-                                                'change': {'rgb': (r,g,b), 'transitiontime':tt, 'on': True}}]
-                                    lightgames.send_msgs(self.connections, \
-                                                {'x':x_grid, 'y':y_grid, 'color':(r,g,b), 'power':True})
-                                else:
-                                    buffer += [{'x': x_grid, 'y': y_grid, 'change': {'on': False}}]
-                                    lightgames.send_msgs(self.connections, \
-                                                {'x':x_grid, 'y':y_grid, 'color':(r,g,b), 'power':False})
-
-                    # only send if theres a lamp to be changed
-                    if buffer:
-                        self.send_lamp_multi(buffer)
-                    yield gen.Task(IOLoop.instance().add_timeout, time.time() + (dur/1000.0))
-            except IOError:
-                print("Couldn't open image")
-
-        # Turning off the lamps here might affect other games
-        #self.send_lamp_all({ 'on': False })
+            self.data.seek(0)
+            self.gif_animator.run_animation(
+                self.data, bounds=self.grid, offset=(self.offset_x, self.offset_y),
+                transitiontime=self.transition_time, transparentcolor=self.transp_color,
+                on_frame=on_frame)
+        except IOError:
+            print("Couldn't open image")
 
     def set_options(self, config):
         files = config['files']
@@ -160,7 +120,7 @@ class GifAnimation(lightgames.Game):
             if fileinfo['content_type'] != 'image/gif':
                 return "Incorrect content type: %s" % fileinfo['content_type']
 
-            self.data = fileinfo['body']
+            self.data = io.BytesIO(fileinfo['body'])
             self.template_vars['animation_file'] = fileinfo['filename']
             self.changed_gif = True
 
@@ -210,13 +170,6 @@ class GifAnimation(lightgames.Game):
             except ValueError:
                 print("Couldn't convert string to int")
 
-        if 'playgif' in config:
-            if config['playgif'] == 'on' and self.play == False:
-                self.play_animation()
-            elif config['playgif'] == 'off':
-                self.play = False
-            self.template_vars['playgif'] = self.play
-
         # Taken from http://code.activestate.com/recipes/266466/
         # Edited for Python 3
         def HTMLColorToRGB(colorstring):
@@ -236,17 +189,13 @@ class GifAnimation(lightgames.Game):
             except ValueError:
                 print("Couldn't convert HTML color to RGB")
 
+        if 'playgif' in config:
+            if config['playgif'] == 'on':
+                self.play_animation()
+            elif config['playgif'] == 'off':
+                self.gif_animator.cancel()
+                self.play = False
+            self.template_vars['playgif'] = self.play
+
         return super().set_options(config)
-
-
-class ImageSequence:
-    def __init__(self, im):
-        self.im = im
-    def __getitem__(self, ix):
-        try:
-            if ix:
-                self.im.seek(ix)
-            return self.im
-        except EOFError:
-            raise IndexError # end of sequence
 
