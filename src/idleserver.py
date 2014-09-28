83b87b21f328e32e74e7876ae22b81ef2d368427
Wamanuz <mackan19@hotmail.com>
2014-09-28 16:09:59 +0200

    Made the idle server use the config file and added a sigint handler

--- src/idleserver.py
+++ src/idleserver.py
@@ -1,19 +1,54 @@
 
 import tornado.ioloop
 import tornado.web 
+import tornado.websocket 
 
+import signal 
+
+import manager 
 
 class SimpleHandler(tornado.web.RequestHandler): 
     def get(self): 
-        self.render('idle.html') 
+        template_vars = {
+
+        }
+        self.render('idle.html', **template_vars) 
+
+
+def initialize():
+    config_file = manager.CONFIG_FILE
+    try:
+        with open(config_file, 'r') as file:
+            cfg = tornado.escape.json_decode(file.read())
+
+        if 'light_pwd' in cfg:
+            manager.light_pwd = cfg['light_pwd']
+            del cfg['light_pwd']
+        if 'config_pwd' in cfg:
+            configinterface.password = cfg['config_pwd']
+            del cfg['config_pwd']
+
+        manager.config.update(cfg)
+    except FileNotFoundError:
+        print("Config file '%s' not found" % config_file)
+    except ValueError as e:
+        print("Error loading config: %s" % e.args[0])
+
+
+initialize() 
 
 handlers = [(r'/', SimpleHandler)]
-
 settings = {'template_path': 'templates', 'static_path': 'static'} 
 
 application = tornado.web.Application(handlers, **settings) 
-application.listen(8080) 
+application.listen(manager.config['serverport']) 
 
-tornado.ioloop.IOLoop.instance().start() 
+loop = tornado.ioloop.IOLoop.instance() 
+
+def on_shutdown(): 
+    loop.stop() 
+
+signal.signal(signal.SIGINT, lambda sig, frame: loop.add_callback_from_signal(on_shutdown)) 
+loop.start() 
 
 
